90a9a24b86d26f237a7473922536a52f0f02059e
otm0937 <otm0937@naver.com>
2023-09-20 12:09:18 +0900

    💡 add feature fuseCameraToLidar projector.m

--- projector.m
+++ projector.m
@@ -8,6 +8,7 @@ cameraSubscriber = rossubscriber("/usb_cam/image_raw");
 load("calibration/camera_cali_result_9.16.20.mat")
 load("calibration/lcc-2023-09-17-00.mat")
 
+player = pcplayer([0, 10], [-5, 5], [-1, 2]);
 while true
     receivedPoints = lidarSubscriber.receive(params);
 
@@ -20,4 +21,7 @@ while true
     hold on
     plot(imPts(:,1),imPts(:,2),'.','Color','r')
     hold off
+
+    ptCloudOut = fuseCameraToLidar(img,receivedPoints,cameraParams,invert(tform));
+    view(player, ptCloudOut)
 end
