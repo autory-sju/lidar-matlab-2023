d51a0e4d27972f261463e2032f3843a7fddb7cef
TK_MINI <40619640+otm0937@users.noreply.github.com>
2023-10-03 23:39:25 +0900

    ✅ add test reconstruction reconstructionTest.m

---
+++ reconstructionTest.m
@@ -0,0 +1,49 @@
+clear; close all; clc; 
+
+ptCloud = pcread('Rosbag_x2023_09_23_16_58_43/600.pcd');
+
+params = lidarParameters('OS1Gen1-32', 1024);
+
+coneW = 400 * 0.001;
+coneH = 700 * 0.001;
+resoultionHorizontal = 45 / (32-1);
+resoultionVertical = 360 / 1024;
+
+filter = @(x, y) coneW*coneH / (8 * norm([x, y])^2 * tand(resoultionVertical/2) * tand(resoultionHorizontal/2));
+
+roi = [0, 20, -10, 10, -1, 2];
+
+roiPoints = getPointsInROI(ptCloud, roi);
+
+figure(OuterPosition=[0,0,560,600])
+pcshow(roiPoints);
+
+nonGroundPoints = getNonGroundSMRF(roiPoints);
+
+[labels,numClusters] = pcsegdist(nonGroundPoints,0.3);
+
+figure(OuterPosition=[560,0,560,600])
+pcshow(nonGroundPoints.Location,labels)
+colormap(hsv(numClusters))
+
+
+mergedPoints = pointCloud([0,0,0]);
+
+for i = 1:numClusters
+    % clustering
+    clusterIndices = find(labels == i);
+    clusterCloud = select(nonGroundPoints, clusterIndices);
+    clusterCenter(:) = mean(clusterCloud.Location);
+
+    % reconstruction
+    indicies = findPointsInCylinder( ...
+        roiPoints,0.2,Center=clusterCenter);
+    pointsInCylinder = select(roiPoints, indicies);
+
+    mergedPoints = pcmerge(mergedPoints, pointsInCylinder, 0.01);
+end
+
+
+figure(OuterPosition=[560*2,0,560,600])
+pcshow(mergedPoints);
+
